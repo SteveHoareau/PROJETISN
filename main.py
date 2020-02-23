c818337ec866c6568dfd3bb39f1add0cddad9426
SteveHoareau <60595412+SteveHoareau@users.noreply.github.com>
2020-02-23 17:29:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #coding:utf-8
-#main.py
+import pygame
+from pygame.locals import *
+
+pygame.init()
+fenetre = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Fla-py Bird !")
+fond = pygame.image.load("images/background.jpg").convert()
+pygame.display.flip()
+continuer = 1
+while continuer:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			continuer = 0
+	
+	fenetre.blit(fond, (0,0))	
+	pygame.display.flip()
